dc26067b8285c50c7430ff6695089e4431b2d333
nekomancer0 <nekomancers0@gmail.com>
2024-08-02 14:16:59 +0200

    cors?

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, "../client")));
 app.use(express.json());
 

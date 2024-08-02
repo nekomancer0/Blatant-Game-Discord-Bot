610ad0b75ea2e6c5bcbde64cbfc8e795d95234cd
nekomancer0 <nekomancers0@gmail.com>
2024-08-02 14:11:23 +0200

    ee

--- client/index.ts
+++ client/index.ts
@@ -1,65 +1,76 @@
-import {initializeSdk} from './utils/initializeSdk';
-import type {MessageData} from './utils/types';
+import { initializeSdk } from "./utils/initializeSdk";
+import type { MessageData } from "./utils/types";
 
-window.addEventListener('DOMContentLoaded', setupParentIframe);
+window.addEventListener("DOMContentLoaded", setupParentIframe);
 
 export async function setupParentIframe() {
   const discordSdk = await initializeSdk();
   notifyChildParentIsReady();
 
-  async function handleMessage({data: messageData}: MessageEvent<MessageData>) {
+  async function handleMessage({
+    data: messageData,
+  }: MessageEvent<MessageData>) {
     // Bail out if messageData is not an "{}" object
-    if (typeof messageData !== 'object' || Array.isArray(messageData) || messageData === null) {
+    if (
+      typeof messageData !== "object" ||
+      Array.isArray(messageData) ||
+      messageData === null
+    ) {
       return;
     }
-    const {nonce, event, command, data, args} = messageData;
+    const { nonce, event, command, data, args } = messageData;
 
-    function handleSubscribeEvent(eventData) {
+    function handleSubscribeEvent(eventData: any) {
       getChildIframe().contentWindow?.postMessage(
         {
           event,
-          command: 'DISPATCH',
+          command: "DISPATCH",
           data: eventData,
         },
-        '*'
+        "*"
       );
     }
 
     switch (command) {
-      case 'NOTIFY_CHILD_IFRAME_IS_READY': {
+      case "NOTIFY_CHILD_IFRAME_IS_READY": {
         notifyChildParentIsReady();
         break;
       }
-      case 'SUBSCRIBE': {
+      case "SUBSCRIBE": {
         if (event == null) {
-          throw new Error('SUBSCRIBE event is undefined');
+          throw new Error("SUBSCRIBE event is undefined");
         }
 
         discordSdk.subscribe(event, handleSubscribeEvent, args);
         break;
       }
-      case 'UNSUBSCRIBE': {
+      case "UNSUBSCRIBE": {
         if (event == null) {
-          throw new Error('UNSUBSCRIBE event is undefined');
+          throw new Error("UNSUBSCRIBE event is undefined");
         }
         discordSdk.unsubscribe(event, handleSubscribeEvent);
         break;
       }
-      case 'SET_ACTIVITY': {
+      case "SET_ACTIVITY": {
         const reply = await discordSdk.commands.setActivity(data as any);
-        getChildIframe().contentWindow?.postMessage({nonce, event, command, data: reply}, '*');
+        getChildIframe().contentWindow?.postMessage(
+          { nonce, event, command, data: reply },
+          "*"
+        );
         break;
       }
     }
   }
 
-  window.addEventListener('message', handleMessage);
+  window.addEventListener("message", handleMessage);
 }
 
 function getChildIframe(): HTMLIFrameElement {
-  const iframe = document.getElementById('child-iframe') as HTMLIFrameElement | null;
+  const iframe = document.getElementById(
+    "child-iframe"
+  ) as HTMLIFrameElement | null;
   if (iframe == null) {
-    throw new Error('Child iframe not found');
+    throw new Error("Child iframe not found");
   }
   return iframe;
 }
@@ -68,9 +79,9 @@ function notifyChildParentIsReady() {
   const iframe = getChildIframe();
   iframe.contentWindow?.postMessage(
     {
-      event: 'READY',
-      command: 'DISPATCH',
+      event: "READY",
+      command: "DISPATCH",
     },
-    '*'
+    "*"
   );
 }
